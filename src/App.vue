df96f7fb1eb1c5aaafdd5243bc6eee1d09313c58
Lanxin2626 <Lanxinzheng20202gmail.com>
2024-10-28 13:11:27 +1100

    add-emit-function

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total"/>
-    <IncomeExpenses />
-    <TransactionList :transactions="transactions" />
-    <AddTransaction />
+    <Balance :total="+total"/>
+    <IncomeExpenses :income="+income" :expense="+expense"/>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleTransactionsDeleted" />
+    <AddTransaction @transactionsSubmitted="handleTransactionsSubmitted" />
   </div>
 </template>
 
@@ -23,8 +23,40 @@ const transactions = ref([
   { id: 3, text: "Book", amount: -10 },
   { id: 4, text: "Camera", amount: 150 },
 ]);
-
+// get Total
 const total = computed(() => {
   return transactions.value.reduce((acc, item) => (acc += item.amount), 0).toFixed(2);
 });
+
+// get Income
+ const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, item) => (acc += item.amount), 0)
+    .toFixed(2);
+  });
+// get Expense
+const expense = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, item) => (acc += item.amount), 0)
+    .toFixed(2);
+});
+// add transaction
+const handleTransactionsSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+};
+// delete transaction
+const handleTransactionsDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction)=> transaction.id !== id);
+};
+// generate unique id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 100000000);
+};
 </script>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,15 +1,38 @@
 <template>
     <h3>Add new transaction</h3>
-    <form id="form">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..." />
+            <input type="text" id="text" v-model="text" placeholder="Enter text..." />
         </div>
         <div class="form-control">
             <label for="amount">Amount <br />
                 (negative - expense, positive - income)</label>
-            <input type="number" id="amount" placeholder="Enter amount..." />
+            <input type="number" id="amount" v-model="amount" placeholder="Enter amount..." />
         </div>
         <button class="btn">Add transaction</button>
     </form>
 </template>
+<script setup>
+import { ref } from "vue";
+
+const text = ref("");
+const amount = ref("");
+
+const emit =defineEmits(['transactionsSubmitted']);
+ const onSubmit = (e) => {
+    if(!text.value || !amount.value){
+        alert('Please add a text and amount');
+    }
+
+    const transactionData = {
+        text: text.value,
+        amount: parseFloat(amount.value),
+    }
+    
+    emit('transactionsSubmitted', transactionData);
+
+    text.value = '';
+    amount.value = '';
+  };
+</script>
